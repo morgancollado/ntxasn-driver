94dd0a2a1784ff4c3c908167ca6bc96c3ae6661f
Morgan Collado <morgan.collado@gmail.com>
2021-03-10 11:50:27 -0600

    creates routes for login and home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import LoginForm from './components/LoginForm'
+import { Route, withRouter, Switch} from 'react-router-dom'
+import {getCurrentUser} from './actions/currentUserActions'
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    this.props.getCurrentUser()
+  }
+  
+  render(){
+   return(
+     <div>
+       <Switch>
+         <Route exact path='/login' component={LoginForm}/>
+         <Route exact path='/' component={Home}/>
+
+       </Switch>
+
+     </div>
+   )
+  }
 }
+const mapStateToProps = state =>{ 
+  if (state.currentUser === null){
+    return ({
+      loggedIn: !!state.currentUser
+    })
 
-export default App;
+    }else {
+      return({
+        loggedIn: state.currentUser,
+        rides: state.currentUser.attributes.driver_rides
+      })
+    }
+  }
+
+export default withRouter(connect(mapStateToProps, {getCurrentUser})(App));
