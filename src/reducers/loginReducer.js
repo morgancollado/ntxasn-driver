a58593cf5f6dbad1448254aebd181959b3a1be06
Morgan Collado <morgan.collado@gmail.com>
2021-03-10 11:57:03 -0600

    creates login reducer

---
+++ src/reducers/loginReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    email: "",
+    password: ""
+}
+
+const loginReducer = (state = initialState, action) => {
+    switch (action.type){
+        case "UPDATE_LOGIN_FORM":
+            return action.formData
+        case "RESET_LOGIN_FORM":
+            return initialState
+        default: 
+            return state
+    }
+}
+
+export default loginReducer
