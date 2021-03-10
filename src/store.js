c81b48a62fb218ac9d88a2b57142032511a62797
Morgan Collado <morgan.collado@gmail.com>
2021-03-10 11:57:18 -0600

    adds login reducer to the store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
 import thunk from 'redux-thunk'
 import currentUserReducer from './reducers/currentUserReducer'
+import loginReducer from './reducers/loginReducer'
 
 const reducer = combineReducers({
-    currentUser: currentUserReducer
+    currentUser: currentUserReducer,
+    login: loginReducer
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
