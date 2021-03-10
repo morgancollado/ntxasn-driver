d612bb25484cc338f8ac63413aae069649b94b60
Morgan Collado <morgan.collado@gmail.com>
2021-03-10 11:22:20 -0600

    links react with the redux store, adds router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {Provider} from 'react-redux'
+import store from './store.js'
+import {BrowserRouter as Router} from 'react-router-dom'
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router><App /></Router>
+  </Provider>,
   document.getElementById('root')
 );
 
