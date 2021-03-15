c5cbcf33e415cfa20753e1c9a2334224d3a53d22
morgancollado <morgan.collado@gmail.com>
2021-03-14 20:09:08 -0500

    creates rides container

---
+++ src/containers/RidesContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MyRides from '../components/MyRides'
+
+const RidesContainer = () => {
+    return(
+        <div className="RidesContainer">
+            <MyRides/>
+        </div>
+    )
+}
+export default RidesContainer
