dcb702188a8fcbe46be7573edd6df62cf01cb7b3
morgancollado <morgan.collado@gmail.com>
2021-03-17 16:53:38 -0500

    updates name of actions

--- src/actions/updateProfileActions.js
+++ src/actions/updateProfileActions.js
@@ -1,12 +1,12 @@
 export const updateProfileForm = (formData) => {
     return {
-        type: "UPDATE_SIGNUP_FORM",
+        type: "UPDATE_PROFILE_FORM",
         formData: formData
     }
 }
 
 export const resetSignupForm = () => {
     return {
-        type: "RESET_SIGNUP_FORM",
+        type: "RESET_PROFILE_FORM",
     }
 }
