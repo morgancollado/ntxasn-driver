9d615e3b8ff79364fcd565a2b68c5d7fe2018644
morgancollado <morgan.collado@gmail.com>
2021-03-27 14:54:19 -0500

    adds form to check after a client has been picked up.

--- src/components/DriverRidePickUp.js
+++ src/components/DriverRidePickUp.js
@@ -5,6 +5,13 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import {Link} from 'react-router-dom'
+import Container from '@material-ui/core/Container';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import {connect} from 'react-redux'
+
 
 const useStyles = makeStyles({
     root: {
@@ -27,9 +34,59 @@ const useStyles = makeStyles({
       const classes = useStyles
 
       return (
-          <div>
-              check in here plz
-          </div>
+        <Card className={classes.root} variant="outlined">
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            My Driver Rides
+          </Typography>
+          <Typography variant="h5" component="h2">
+          Appointment Date and Time: {ride.attributes.date_time}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+          Appointment Type: {ride.attributes.appointment_type}
+          </Typography>
+          <Typography variant="body2" component="p">
+          Location From: {ride.attributes.location_from}
+            <br />
+            Location To: {ride.attributes.location_to}
+          </Typography>
+          <Typography variant="body2" component="p">
+          Passenger: {ride.attributes.passenger ? ride.attributes.passenger.name : "Your Driver information will appear here once a driver is assigned"} 
+            <br />
+            Phone Number: {ride.attributes.passenger ? ride.attributes.passenger.phone_number: "Your Driver information will appear here once a driver is assigned"}
+          </Typography>
+          <Typography variant="body2" component="p">
+        
+            Email: {ride.attributes.passenger ? ride.attributes.passenger.email : "Your Driver information will appear here once a driver is assigned"}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Container>
+              <form>
+                <RadioGroup aria-label="Picked up?" name="gender1" >
+                    <FormControlLabel value="female" control={<Radio />} label="Picked up?" />
+                </RadioGroup>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                >
+                    Submit
+                </Button>
+
+              </form>
+
+          </Container>
+          <Link to={`/rides/${ride.id}/initialpickup`}>
+            <Button> 
+              Please click here after you have picked up the client.
+            </Button>
+          </Link>
+  
+        </CardActions>
+      </Card>
       )
   }
   export default DriverRidePickUp
