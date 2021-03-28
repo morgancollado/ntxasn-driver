1e31bf38b2830809d4e08cc5c1444371797481f9
morgancollado <morgan.collado@gmail.com>
2021-03-27 20:15:32 -0500

    adds update ride case statement to update status of ride

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -4,6 +4,15 @@ const currentUserReducer = (state = null, action) => {
             return action.user
         case "CLEAR_CURRENT_USER":
             return null
+        case "UPDATE_RIDE":
+            let driver_rides = state.attributes.driver_rides.map(ride => ride.id === action.ride.id ? action.ride : ride)
+                return {
+                    ...state,
+                    attributes: {
+                        ...state.attributes,
+                        driver_rides
+                    }
+                }
         default: 
             return state
     }
